075d70883c52cbeb4fae199a778cb92e9fc59889
Wesley Westelley <wesleywestelley@gmail.com>
2021-10-15 15:47:52 -0300

    :hammer: feat: added routes in application and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Profile from "./pages/Profile";
+import Repo from "./pages/Repo";
+import GlobalStyles from "./styles/GlobalStyles";
+import "react-calendar-heatmap/dist/styles.css";
+import Footer from "./components/Footer";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+
+import { ThemeName, themes } from "./styles/themes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [themeName, setThemeName] = useState<ThemeName>("light");
+    const currentTheme = themes[themeName];
+
+    return (
+        <ThemeProvider theme={currentTheme}>
+            <BrowserRouter>
+                <Header themeName={themeName} setThemeName={setThemeName} />
+                <Routes>
+                    <Route path="/" element={<Profile />} />
+                    <Route path="/:username" element={<Profile />} />
+                    <Route path="/:username/:reponame" element={<Repo />} />
+                </Routes>
+                <Footer />
+                <GlobalStyles />
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;
